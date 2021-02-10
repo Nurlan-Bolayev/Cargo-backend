5c22fb23f1a7c80afbd0b4a65b9a8fdb999112fd
Nurlan Bolayev <bolayev.nurlan@gmail.com>
2021-02-10 14:28:57 +0400

    Update UserFactory file

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,8 +24,16 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name,
+            'surname' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
+            'birth_date' => '1996/10/24',
+            'ID_card_number' => 13404269,
+            'IIN' => '6EXZZ6V',
+            'gender' => rand(0,1) ? 'male' : 'female',
+             'address' => $this->faker->address,
+            'delivery_address'=> $this->faker->address,
             'email_verified_at' => now(),
+            'phone_number' => '+99455-462-89-54',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
